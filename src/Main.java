7dbf6a14c86d0987193caa93261783612a300330
Gabseek <125920170+Gabseek@users.noreply.github.com>
2024-10-18 19:09:37 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        String filename = "C:\\codigos\\Fatorial.txt";
-        try{
+        String filename = "C:\\codigos\\Fatorial (Com erros).txt";  
+
+        try {
             CharStream input = CharStreams.fromFileName(filename);
-            TupyLexer lexer = new TupyLexer(input);
+            TupyLexer lexer = new TupyLexer(input); 
             Token token;
-            while (!lexer._hitEOF){
-                token = lexer.nextToken();
-                System.out.println("Token: <Classe: "+lexer.getVocabulary().getSymbolicName(token.getType()) +" ,Lexema: "+ token.getText() +">");
+
+            while ((token = lexer.nextToken()).getType() != Token.EOF) {
+                String tokenType = lexer.getVocabulary().getSymbolicName(token.getType());
+
+                if (tokenType.equals("EOF")) {
+                    break;
+                } else if (tokenType == null || tokenType.isEmpty()) {
+                    System.err.println("Erro: Token inválido na linha " + token.getLine() + " com lexema: \"" + token.getText() + "\"");
+                } else {
+                    System.out.println("Token: <Classe: " + tokenType + " ,Lexema: " + token.getText() + ">");
+                }
             }
 
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
